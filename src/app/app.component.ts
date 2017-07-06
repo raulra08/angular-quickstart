0e1579338ec990bbdfabbc910b27019f559f2a59
Raul Rodriguez <raul182ra@hotmail.com>
2017-07-06 18:09:24 +0100

    Working copy of Hero Editor party 1 https://angular.io/tutorial/toh-pt1 But adapted to show an application's snapshot version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,29 @@
 import { Component } from '@angular/core';
 
 @Component({
+
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{application.name}} details!</h2>
+    <div><label>id: </label>{{application.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="application.name" placeholder="name">
+    </div>
+  `
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent  {
+  title = 'Digital Security Platform';
+
+  application: Application = {
+    id: 1,
+    name: 'Snapshot Version'
+  };
+}
+
+export class Application {
+  id: number;
+  name: string;
+}
